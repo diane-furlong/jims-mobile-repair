4be687db75eb682aba32253eeff789c4d11f399c
diane-furlong <dfurlongm@gmail.com>
2021-08-13 22:36:06 -0400

    changing container

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -4,7 +4,7 @@ import {Col, Row, Container} from 'react-bootstrap'
 
 const home = () => {
     return (
-        <Container className="mainDiv">
+        <div className="mainDiv">
             <h1 className="title">Jim's Mobile Repair</h1>
             <h3 className="title">Stay put- I'll come to you!</h3>
             <br/>
@@ -12,26 +12,28 @@ const home = () => {
             <h4 className="intro">I can fix just about anything.</h4>
             <br/>
             <br/>
-            <Row className="homeLists">
-                <Col md={5} className="specialize">
-                    <h5>I specialize in:
-                    <ul>
-                        <li>Engine Repair</li>
-                        <li>Welding</li>
-                    </ul></h5>
-                </Col>
-                <Col md={5} className="fixExamples">
-                    <h5>I can fix your:
-                    <ul>
-                        <li>Car</li>
-                        <li>Watercraft</li>
-                        <li>Lawnmower</li>
-                        <li>Snowblower</li>
-                        <li>Snow Plow</li>
-                        <li>And a variety of other engines. <br/> To ask about a specific item, <a href="/contact">contact me</a>!</li>
-                    </ul></h5>
-                </Col>
-            </Row>
+            <Container>
+                <Row className="homeLists">
+                    <Col md={5} className="specialize">
+                        <h5>I specialize in:
+                        <ul>
+                            <li>Engine Repair</li>
+                            <li>Welding</li>
+                        </ul></h5>
+                    </Col>
+                    <Col md={5} className="fixExamples">
+                        <h5>I can fix your:
+                        <ul>
+                            <li>Car</li>
+                            <li>Watercraft</li>
+                            <li>Lawnmower</li>
+                            <li>Snowblower</li>
+                            <li>Snow Plow</li>
+                            <li>And a variety of other engines. <br/> To ask about a specific item, <a href="/contact">contact me</a>!</li>
+                        </ul></h5>
+                    </Col>
+                </Row>
+            </Container>
             <br/>
             <h5 className="repairContact">
             What can I repair for you?
@@ -39,7 +41,7 @@ const home = () => {
             <button className="repairContactBtn" href="/contact">
                 Contact Me
             </button>
-        </Container>
+        </div>
     )
 }
 
